a14d161f5e6ec371183a6265cf2a15bca9d3872d
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2024-09-23 19:25:52 +0800

    Fix rendering order for implication and biconditional

--- ui/src/main/scala/com/melvic/lohika/ui/symbols/Symbols.scala
+++ ui/src/main/scala/com/melvic/lohika/ui/symbols/Symbols.scala
@@ -8,13 +8,21 @@ trait Symbols:
   def Iff: String
   def Entailment: String
 
+  /**
+   * Maps texts to symbols.
+   *
+   * Note: The ordering matters here, at least for some of them. In particular, `|=` should come
+   * before `|` and `<=>` should come before `=>`. In other words, the longer symbols should be
+   * prioritized to avoid rendering issues. If, for instance, `|` precedes `|=`, then `|=` will be
+   * rendered as the symbol for `|` followed by `=`.
+   */
   def TextToSymbolsMap: List[(String, String)] = List(
     "|="  -> Entailment,
     "&"   -> And,
     "|"   -> Or,
     "!"   -> Not,
-    "=>"  -> Imply,
-    "<=>" -> Iff
+    "<=>" -> Iff,
+    "=>"  -> Imply
   )
 
   def applyToText(text: String): String =
