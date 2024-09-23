fd061702aa7c861db2e0811d94e42cb2be5b02d0
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2024-09-24 00:50:36 +0800

    Add more proof test cases

--- core/src/main/scala/com/melvic/lohika/prover/algebras/Prover.scala
+++ core/src/main/scala/com/melvic/lohika/prover/algebras/Prover.scala
@@ -2,10 +2,11 @@ package com.melvic.lohika.prover.algebras
 
 import cats.*
 import cats.implicits.*
-import com.melvic.lohika.Cnf.Clause
+import com.melvic.lohika.Cnf.{CNot, CVar, Clause}
 import Prover.ResolutionResult
 import com.melvic.lohika.{Clauses, Cnf, Entailment}
 import com.melvic.lohika.formula.Formula
+import com.melvic.lohika.parsers.Lexemes
 
 trait Prover[F[_]]:
   def parseEntailment(rawEntailment: String): F[Entailment]
@@ -34,5 +35,18 @@ object Prover:
   final case class Derive(left: Clause, right: Clause, result: Clause)
   final case class Contradiction(clause1: Clause, clause2: Clause)
   case object Exhaustion
+
+  object Contradiction:
+    /**
+     * Creates a contradiction (e.g. P & !P) from the variable name. If the input is in the form
+     * `!P`, the resulting contradiction is `!P & P`. Otherwise, it is `P & !P`
+     */
+    def fromVarName(varName: String): Contradiction =
+      if varName.startsWith(Lexemes.Not) then
+        val variable = CVar(varName.tail)
+        Contradiction(CNot(variable), variable)
+      else
+        val variable = CVar(varName)
+        Contradiction(variable, CNot(variable))
 
   def apply[F[_]](using prover: Prover[F]): Prover[F] = prover
--- core/src/test/scala/com/melvic/lohika/tests/ProofSpec.scala
+++ core/src/test/scala/com/melvic/lohika/tests/ProofSpec.scala
@@ -10,43 +10,77 @@ import com.melvic.lohika.prover.programs.ProverProgram
 
 class ProofSpec extends AnyFlatSpec with should.Matchers:
   "A" should "be provable from A | B" in:
-    result("A |= A | B") should be(Right(Contradiction(CVar("A"), CNot(CVar("A")))))
+    contradiction("A |= A | B", "A")
 
   "A & C" should "not be provable from A & !C" in:
-    result("A & !C |= A & C") should be(Right(Exhaustion))
+    exhaustion("A & !C |= A & C")
 
   "A | B" should "be provable from A & C" in:
-    result("A & C |= A | B") should be(Right(Contradiction(CVar("A"), CNot(CVar("A")))))
+    contradiction("A & C |= A | B", "A")
 
   "C" should "be provable from A => B, B => C, and A" in:
-    result("A => B, B => C, A |= C") should be(Right(Contradiction(CNot(CVar("C")), CVar("C"))))
+    contradiction("A => B, B => C, A |= C", "!C")
 
   "P | R" should "be provable from P | Q, !Q | R" in:
-    result("P | Q, !Q | R |= P | R") should be(Right(Contradiction(CVar("R"), CNot(CVar("R")))))
+    contradiction("P | Q, !Q | R |= P | R", "R")
 
   "A | C" should "not be provable from A => B, and B | C" in:
-    result("A => B, B | C |= A | C") should be(Right(Exhaustion))
+    exhaustion("A => B, B | C |= A | C")
 
   "Q" should "be provable from !P | Q and P" in:
-    result("!P | Q, P |= Q") should be(Right(Contradiction(CNot(CVar("P")), CVar("P"))))
+    contradiction("!P | Q, P |= Q", "!P")
 
   "R" should "be provable from P | Q, !Q | R, and !P" in:
-    result("P | Q, !Q | R, !P |= R") should be(Right(Contradiction(CVar("R"), CNot(CVar("R")))))
+    contradiction("P | Q, !Q | R, !P |= R", "R")
 
   "B | C" should "be provable from A | B, !A" in:
-    result("A | B, !A |= B | C") should be(Right(Contradiction(CVar("B"), CNot(CVar("B")))))
+    contradiction("A | B, !A |= B | C", "B")
 
   "A | !C" should "be provable from A => B and B => C" in:
     result("A => B, B => C |= A | !C") should be(Right(Exhaustion))
 
   "P | !P" should "be a tautology" in:
-    result("P | !P") should be(Right(Contradiction(CNot(CVar("P")), CVar("P"))))
+    contradiction("P | !P", "!P")
 
   "(P & Q) => P" should "be a tautology" in:
-    result("P | !P") should be(Right(Contradiction(CNot(CVar("P")), CVar("P"))))
+    contradiction("P | !P", "!P")
 
   "(P | Q) & (!Q => R) => (P => R)" should "not be provable from P | Q and !Q => R" in:
-    result("P | Q, !Q => R |= (P | Q) & (!Q => R) => (P => R)") should be(Right(Exhaustion))
+    exhaustion("P | Q, !Q => R |= (P | Q) & (!Q => R) => (P => R)")
+
+  "P => Q, Q <=> R, P |= R" should "hold" in:
+    contradiction("P => Q, Q <=> R, P |= R", "R")
+
+  "A => B, B => C, !C |= !A" should "hold" in:
+    contradiction("A => B, B => C, !C |= !A", "!C")
+
+  "P <=> Q, Q => R, P |= R" should "hold" in:
+    contradiction("P <=> Q, Q => R, P |= R", "R")
+
+  "P => Q, Q <=> R, !R |= !P" should "hold" in:
+    contradiction("P => Q, Q <=> R, !R |= !P", "R")
+
+  "P <=> Q, Q => R, R |= P" should "not hold" in:
+    exhaustion("P <=> Q, Q => R, R |= P")
+
+  "P => Q, R => P, R |= Q" should "hold" in:
+    contradiction("P => Q, R => P, R |= Q", "Q")
+
+  "P => Q, Q => R, R => S, P |= S" should "hold" in:
+    contradiction("P => Q, Q => R, R => S, P |= S", "!P")
+
+  // [Showcase]
+  "A => B, B => C, C => D, !D |= !A" should "hold" in:
+    contradiction("A => B, B => C, C => D, !D |= !A", "D")
+
+  "X => Y, Y => Z, Z => W, W |= X" should "not hold" in:
+    exhaustion("X => Y, Y => Z, Z => W, W |= X")
+
+  def contradiction(entailment: String, varName: String): Unit =
+    result(entailment) should be(Right(Contradiction.fromVarName(varName)))
+
+  def exhaustion(entailment: String): Unit =
+    result(entailment) should be(Right(Exhaustion))
 
 object ProofSpec:
   def result(entailment: String): Either[String, ResolutionResult] =
