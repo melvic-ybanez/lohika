dcaf8469004da151652bdc018eca38784f0448a0
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2024-10-14 03:07:02 +0800

    Add more test cases for skolemization

--- core/src/test/scala/com/melvic/lohika/formula/SkolemizationSpec.scala
+++ core/src/test/scala/com/melvic/lohika/formula/SkolemizationSpec.scala
@@ -6,17 +6,23 @@ import com.melvic.lohika.formula.FormulaMappingSupport.{====>, FormulaMapper}
 
 class SkolemizationSpec extends BaseSpec with FormulaMappingSupport:
   "Skolemization" should "replace existentially quantified variables with Skolem functions" in:
-    "A:xE:yP(x, y)" ====> "A:xP(x, f(x))"
+    "A:xE:yP(x, y)"       ====> "A:xP(x, f(x))"
+    "A:xE:y(P(x) | Q(y))" ====> "A:x(P(x) | Q(f(x)))"
+
+  it should "replace variables with Skolem constants when there's no preceding universal quantifier" in:
+    "E:xA:yP(x, y)" ====> "A:yP(x, y)"
 
   it should "account for all universal quantifiers that come before an existential quantifier" in:
-    "A:xA:zE:yP(x, y, z)"         ====> "A:xA:zP(x, f(x, z), z)"
-    "A:x,yE:zA:aE:b(P(a) & Q(b))" ====> "A:x,yA:a(P(a) & Q(f(x, y, a)))"
-    "A:xE:yA:zE:wP(x, y, z, w)" ====> "A:xA:zP(x, f(x), z, g(x, z))"
+    "A:xA:zE:yP(x, y, z)"                ====> "A:xA:zP(x, f(x, z), z)"
+    "A:x,yE:zA:aE:b(P(a) & Q(b))"        ====> "A:x,yA:a(P(a) & Q(f(x, y, a)))"
+    "A:xE:yA:zE:wP(x, y, z, w)"          ====> "A:xA:zP(x, f(x), z, g(x, z))"
+    "A:xA:yE:zE:w(P(x, y, z) & Q(z, w))" ====> "A:xA:y(P(x, y, f(x, y)) & Q(f(x, y), g(x, y)))"
+    "E:xA:yE:zE:w(R(x, y) | (S(z, w) & T(y)))" ====> "A:y(R(x, y) | (S(f(y), g(y)) &  T(y)))"
 
   it should "work on nested existential quantifiers" in:
-    "A:xE:yE:z(P(x, y) & Q(y, z))" ====> "A:x(P(x, f(x)) & Q(f(x), g(x)))"
-    "A:xE:yE:z(P(x, y) & Q(z, y))" ====> "A:x(P(x, f(x)) & Q(g(x), f(x)))"
-    "A:xE:yE:z(P(y, z) & Q(x, y))" ====> "A:x(P(f(x), g(x)) & Q(x, f(x)))"
+    "A:xE:yE:z(P(x, y) & Q(y, z))"      ====> "A:x(P(x, f(x)) & Q(f(x), g(x)))"
+    "A:xE:yE:z(P(x, y) & Q(z, y))"      ====> "A:x(P(x, f(x)) & Q(g(x), f(x)))"
+    "A:xE:yE:z(P(y, z) & Q(x, y))"      ====> "A:x(P(f(x), g(x)) & Q(x, f(x)))"
     "A:xE:y,aE:z(P(y, z, a) & Q(x, y))" ====> "A:x(P(f(x), h(x), g(x)) & Q(x, f(x)))"
 
   override given formulaMapper: FormulaMapper =
