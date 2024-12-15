6a1857879e6ccb455cd182c916364146c358ba5c
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2024-12-16 01:58:58 +0800

    Support loading of Lohika scripts

--- controllers/src/main/scala/com/melvic/lohika/controllers/FileManager.scala
+++ controllers/src/main/scala/com/melvic/lohika/controllers/FileManager.scala
@@ -1,22 +1,28 @@
 package com.melvic.lohika.controllers
 
 import java.io.*
+import scala.io.Source
 import scala.util.{Try, Using}
 
 trait FileManager:
   def save(script: String, path: String): Try[File]
+
+  def open(path: String): Try[String]
 
 object FileManager:
   val FileExtension: String = ".lhk"
 
   def live: FileManager = LiveFileManager()
 
-  def toFileWithExtension(path: String): File =
+  def toExtendedFile(path: String): File =
     File(if path.endsWith(FileExtension) then path else path + FileExtension)
 
 class LiveFileManager extends FileManager:
   def save(script: String, path: String): Try[File] =
-    val fileWithExtension = FileManager.toFileWithExtension(path)
-    Using(BufferedWriter(FileWriter(fileWithExtension, false))): writer =>
+    val extendedFile = FileManager.toExtendedFile(path)
+    Using(BufferedWriter(FileWriter(extendedFile, false))): writer =>
       writer.write(script)
-      fileWithExtension
+      extendedFile
+
+  override def open(path: String): Try[String] =
+    Using(Source.fromFile(path))(_.mkString)
--- ui/src/main/scala/com/melvic/lohika/ui/EditorTabPane.scala
+++ ui/src/main/scala/com/melvic/lohika/ui/EditorTabPane.scala
@@ -1,16 +1,44 @@
 package com.melvic.lohika.ui
 
+import javafx.scene.control.Tab
 import scalafx.Includes.*
+import scalafx.application.Platform
 import scalafx.beans.property.StringProperty
-import scalafx.scene.control.TabPane.TabClosingPolicy.Unavailable
-import scalafx.scene.control.{Tab, TabPane}
+import scalafx.scene.control.TabPane
 
 class EditorTabPane(mainScene: MainScene) extends TabPane:
-  tabClosingPolicy = Unavailable
+  def openUntitled(): Unit =
+    openTab("Untitled", "", "")
 
-  val tab: Tab = new Tab:
-    content = EditorPane(mainScene)
+  def openTab(title: String, script: String, path: String): Unit =
+    val tab = EditorTab(mainScene)
+    tab.setText(title)
+    tab.editorPane.withContent(script)
+    tab.pathProp.set(path)
+    tabs.addOne(tab)
+    selectionModel.value.select(tab)
 
-  tab.textProperty() <== mainScene.selectedTitleProp
+  def withSelectedTitle(title: String): EditorTabPane =
+    selectedEditorTab.text = title
+    this
 
-  tabs = List(tab)
+  def withSelectedContent(content: String): EditorTabPane =
+    selectedEditorTab.editorPane.withContent(content)
+    this
+
+  def selectedEditorTab: EditorTab =
+    selectionModel.value.getSelectedItem.asInstanceOf[EditorTab]
+
+  def selectedPathProp: StringProperty =
+    selectedEditorTab.pathProp
+
+  def selectedTitleProp: StringProperty =
+    selectedEditorTab.textProperty()
+
+class EditorTab(mainScene: MainScene) extends Tab:
+  val editorPane = EditorPane(mainScene)
+  val pathProp: StringProperty = StringProperty("")
+
+  setContent(editorPane)
+  setOnCloseRequest: _ =>
+    if getTabPane.getTabs.size == 1 then Platform.exit()
